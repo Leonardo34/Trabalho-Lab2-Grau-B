f939f3a023b25d4e0bbfb73a3743eb02b6a37d5b
Leonardo <leo_broch@hotmail.com>
2017-05-31 16:36:25 -0300

    Implementar core do menu de operações

---
+++ src/br/com/unisinos/trabGrauB/Teclado.java
@@ -0,0 +1,84 @@
+package br.com.unisinos.trabGrauB;
+
+import java.io.*;
+
+public class Teclado {
+	private String s;
+	private InputStreamReader i = new InputStreamReader(System.in);
+	private BufferedReader d = new BufferedReader(i);
+
+	public int leInt() {
+		int a = 0;
+		try {
+			s = d.readLine();
+			a = Integer.parseInt(s);
+		} catch (IOException e) {
+			System.out.println("Erro de I/O: " + e);
+		} catch (NumberFormatException e) {
+			System.out.println("o valor digitado deve ser inteiro: " + e);
+		}
+		return (a);
+	}
+
+	public int leInt(String msg) {
+		int a = 0;
+		System.out.println(msg);
+		try {
+			s = d.readLine();
+			a = Integer.parseInt(s);
+		} catch (IOException e) {
+			System.out.println("Erro de I/O: " + e);
+		} catch (NumberFormatException e) {
+			System.out.println("o valor digitado deve ser inteiro: " + e);
+		}
+		return (a);
+	}
+
+	public double leDouble() {
+		double a = 0;
+		try {
+			s = d.readLine();
+			a = Double.parseDouble(s);
+		} catch (IOException e) {
+			System.out.println("Erro de I/O: " + e);
+		} catch (NumberFormatException e) {
+			System.out.println("o valor digitado deve ser n�mero: " + e);
+		}
+		return (a);
+	}
+
+	public double leDouble(String msg) {
+		double a = 0;
+		System.out.println(msg);
+		try {
+			s = d.readLine();
+			a = Double.parseDouble(s);
+		} catch (IOException e) {
+			System.out.println("Erro de I/O: " + e);
+		} catch (NumberFormatException e) {
+			System.out.println("o valor digitado deve ser numero: " + e);
+		}
+		return (a);
+	}
+
+	public String leString() {
+		s = "";
+		try {
+			s = d.readLine();
+		} catch (IOException e) {
+			System.out.println("Erro de I/O: " + e);
+		}
+		return (s);
+	}
+
+	public String leString(String msg) {
+		s = "";
+		System.out.println(msg);
+		try {
+			s = d.readLine();
+		} catch (IOException e) {
+			System.out.println("Erro de I/O: " + e);
+		}
+		return (s);
+	}
+}
--- src/br/com/unisinos/trabGrauB/TestClass.java
+++ src/br/com/unisinos/trabGrauB/TestClass.java
@@ -2,15 +2,35 @@ package br.com.unisinos.trabGrauB;
 
 public class TestClass {
 	public static void main(String[] args) {
-		DoublyLinkedList<Cell> lista = new DoublyLinkedList<>();
-		Cell celula = new FormulaCell("=A1 A2 +", lista, "Pi");
-		Cell celula2 = new FormulaCell("=A2 5 +", lista , "A1");
-		Cell celula3 = new ValueCell(5, "A2");
-		lista.insertFirst(celula);
-		lista.insertLast(celula2);
-		lista.insertLast(celula3);
-		System.out.println("testeeee");
-		System.out.println(celula.eval());
-		//System.out.println("+".matches("[-+*/^]"));
+		DoublyLinkedList<Cell> planilha = new DoublyLinkedList<>();
+		Teclado tecladoReader = new Teclado();
+		int opcao;
+		
+		do {
+			imprimirMenu();
+			opcao = tecladoReader.leInt();
+			
+			if (opcao == 1) {
+				String id = tecladoReader.leString("Digite o id da nova celula: ");
+				String valor = tecladoReader.leString("Digite o valor da celula");
+				if (valor.charAt(0) == '=') {
+					planilha.insertLast(new FormulaCell(valor, planilha, id));
+				} else {
+					planilha.insertLast(new ValueCell(Double.parseDouble(valor), id));
+				}
+			}
+			
+		} while (opcao != 9);
+	}
+	
+	public static void imprimirMenu() {
+		System.out.println("Digite a opcao:");
+		System.out.println("1 - Incluir célula");
+		System.out.println("2 - Avaliar célula");
+		System.out.println("3 - Avaliar todas as células");
+		System.out.println("4 - Conteúdo de todas as células");
+		System.out.println("5 - Salvar em arquivo");
+		System.out.println("6 - Abrir arquivo");
+		System.out.println("9 - Sair");
 	}
 }
